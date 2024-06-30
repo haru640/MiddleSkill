430b7d134b51068a671f9c146265029c9522d85b
haru640 <136230611+haru640@users.noreply.github.com>
2024-06-30 22:54:53 +0900

    1-9

---
+++ 1-9/src/test1.java
@@ -0,0 +1,31 @@
+
+	   public class test1 {
+        public static void main(String[] args) {
+        
+//      1.数字の5と9の変数を定義し、足した数字を出力しなさい。
+        int n1 = 5;
+        int n2 = 9;
+        
+        int result = n1+n2;
+        System.out.println(result);
+
+//      2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
+        long num = 1111111111;
+        long result1 = num * 2;
+        System.out.println(result1);
+        
+//      3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。
+      
+         boolean isValid = false;
+
+         if (!isValid) {
+         System.out.println(false);
+          } 
+         
+         else {
+         System.out.println(true); 
+                }
+            }
+        }
+
+
