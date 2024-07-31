84f7e21c6e60b460826dd8cbc0e44ddf110b9ec2
haru640 <136230611+haru640@users.noreply.github.com>
2024-07-31 21:36:02 +0900

    1-13

--- 1-13/src/test1.java
+++ 1-13/src/test1.java
@@ -1,45 +1,58 @@
+	        
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 	public class test1 {
-		public static void main(String[] args) {
-			//改行
-			System.out.println("問１");
-			//問題1:下記の条件を使ってfor文と条件文を組み合わせ結果が同じになるよう出力しなさい。for文ネスト3回（3回forを書くこと）
-//			例）
-//			for() {
-//				for() {
-//					for() {
-//						
-//					}
-//				}
-//			}
-			//strArray1でfor文を回し3文字目(k)以降のものをstrArray2に格納する(1個目のfor文)
-			//strArray2でfor文を回しkとaとoの物だけでstrArray3に格納する(2個目のfor文)
-			//strArray3でfor文を回し「kakao」となるようにstrArray4に格納しstrArray4を出力する。(3個目のfor文)
-			List<String> strArray1 = new ArrayList<String>(Arrays.asList("f", "u", "k", "u", "d", "a", "k" , "a" , "n" , "e" , "k" , "o" ));
-			List<String> strArray2 = new ArrayList<String>();
-			List<String> strArray3 = new ArrayList<String>();
-			List<String> strArray4 = new ArrayList<String>();		
-			
-			 for (int i = 2; i < strArray1.size(); i++) {
-	            String str = strArray1.get(i);
-	            strArray2.add(str);
-	        }
+	    public static void main(String[] args) {
+	    	System.out.println("問１");
+	        List<String> strArray1 = new ArrayList<>(Arrays.asList("f", "u", "k", "u", "d", "a", "k", "a", "n", "e", "k", "o"));
+	        List<String> strArray2 = new ArrayList<>();
+	        List<String> strArray3 = new ArrayList<>();
+	        List<String> strArray4 = new ArrayList<>();
 
-	        for (String str : strArray2) {
-	            if (str.equals("k") || str.equals("a") || str.equals("o")) {
-	                strArray3.add(str);
+	        for (int i = 0; i < strArray1.size(); i++) {
+	            for (int j = 0; j < strArray1.size(); j++) {
+	                for (int k = 0; k < strArray1.size(); k++) {
+	                    if (i >= 2) {
+
+	                        if (j == i && k == 0) {
+	                            strArray2.add(strArray1.get(i));
+	                        }
+	                    }
+	                }
 	            }
 	        }
 
-	        for (int i = 0; i < strArray3.size() - 3; i++) {
-	            if (strArray3.get(i).equals("k") && strArray3.get(i + 1).equals("a") && strArray3.get(i + 2).equals("k") && strArray3.get(i + 3).equals("o")) {
-	                strArray4.add("kakao");
+	        for (int i = 0; i < strArray2.size(); i++) {
+	            for (int j = 0; j < strArray2.size(); j++) {
+	                for (int k = 0; k < strArray2.size(); k++) {
+	                    String s = strArray2.get(i);
+	                    if (s.equals("k") || s.equals("a") || s.equals("o")) {
+	                        if (j == i && k == 0) {
+	                            strArray3.add(s);
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        for (int i = 0; i < strArray3.size(); i++) {
+	            for (int j = 0; j < strArray3.size(); j++) {
+	                for (int k = 0; k < strArray3.size(); k++) {
+	                    if (strArray3.size() >= 5) {
+	                        if (i == 0 && j == 1 && k == 2) {
+	                            strArray4.add(strArray3.get(i));
+	                            strArray4.add(strArray3.get(j));
+	                            strArray4.add(strArray3.get(k));
+	                            strArray4.add("a");
+	                            strArray4.add("o");
+	                        }
+	                    }
+	                }
 	            }
 	        }
 
-	        	    System.out.println(strArray4);
+	        System.out.println("Result: " + String.join("", strArray4));
 	    }
 	}
 	
